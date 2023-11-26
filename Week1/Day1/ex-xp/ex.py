ea78b1f5731f5d3bb5e0c1d07f5a505e0c43e21f
zh777by <byalo.ev@gmail.com>
2023-11-26 15:26:05 +0200

    relevant message

--- Week1/Day1/ex-xp/ex.py
+++ Week1/Day1/ex-xp/ex.py
@@ -5,3 +5,48 @@
 # print((99**3)*8)
 
 # ex3
+# print('1st', 5 > 3)
+# print('2st', 3 == 3)
+# print('3st', 3 == "3")
+# print('4st', int("3") > 3)
+# print('5st', "Hello" == "hello")
+
+# ex4
+# computer_brand = 'Apple'
+# print(f"I have an {computer_brand} computer" )
+
+#ex5
+# name = 'Evgenii'
+# age = 43
+# shoe_size = 42
+# info = (f'My name is {name}, I am {age} and my shoe_size is {shoe_size}')
+# print(info)
+
+#ex6
+# a = int(input('Enter a number: '))
+# b = int(input('Enter a number: '))
+# if a > b: 
+#     print('Hello word')
+
+#ex7
+# user_input = input('Enter a number: ')
+# number = int(user_input)
+# if number % 2 == 0:
+#     print(f'{number} is an even number.')
+# else:
+#     print(f'{number} is an odd number.')
+
+#ex8
+# My_name = 'Evgenii'
+# name = input("What is your name ")
+# if My_name == name:
+#     print('Our names are same!')
+# else:
+#     print('Our names are not the same!')
+
+#ex9
+
+
+
+
+
