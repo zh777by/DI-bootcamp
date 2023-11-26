5b21e6b92ebb4c395adccd55ba8c99c7751462d5
zh777by <byalo.ev@gmail.com>
2023-11-26 19:32:04 +0200

    relevant message

---
+++ Week1/Day1/Daily Chalenge/d_ch.py
@@ -0,0 +1,10 @@
+user_input = input("Enter a string (must be 10 characters long): ")
+# if len(user_input) < 10:
+#     print('string not long enough') 
+# elif len(user_input) > 10:
+#     print('string too long')
+# else:
+#     print('perfect string')
+
+# print('First character:', user_input[0])
+# print('Last character:', user_input[-1])
