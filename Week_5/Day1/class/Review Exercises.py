76fc5ffb05420349e395ecc3aa4daee77a4bc126
CharlotteBenadiba <142738444+CharlotteBenadiba@users.noreply.github.com>
2023-12-18 11:24:11 +0100

    Update Review Exercises.py

--- Week_5/Day1/class/Review Exercises.py
+++ Week_5/Day1/class/Review Exercises.py
@@ -17,7 +17,7 @@
 #     else:
 #         print(string[::-1])
 
-
+###########Incorrect. The code currently prints all names in the list. To print every other name, it should use slicing (names[::2]).
 #3. Write code for a list that contains four names and prints every other name.
 
 # names = ["Anna", "Tim", "Mark", "Olga"]
@@ -50,7 +50,7 @@
 # }
 # print(my_dict)
 
-
+##########Almost correct. The input for blood_alcohol_level should be a float, not an int, as blood alcohol levels are typically fractional values.
 #6. A user is allowed to drive home if their blood alcohol is less than 0.5 %. 
 #Ask a user for their blood alcohol level and if they're not sober, tell them to take a cab.
 
