2eb66b1d407dda1a1e9f70ff8392e1f68a50085c
zh777by <byalo.ev@gmail.com>
2023-12-31 14:59:06 +0200

    relevant message

---
+++ Week_7/Day1/class/class.py
@@ -0,0 +1,5 @@
+# https://colab.research.google.com/drive/16gWbqwQwGQJ_zg8-3lJ19JMi8GBmepq8#scrollTo=5glPNdsF7GVP
+
+# https://colab.research.google.com/drive/1EMWbfZmKLM4bOsnXE5tFp7068zPqHzjz#scrollTo=f0455702
+
+# https://colab.research.google.com/drive/1RL9ifZWq2WGO5iakQ7oijNsZj01vGot-
