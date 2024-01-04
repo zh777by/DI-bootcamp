e88b40044560b79b1071646694e4b5de52fe1649
zh777by <byalo.ev@gmail.com>
2024-01-04 14:40:58 +0200

    relevant message

---
+++ Week_7/Day3/class/class.py
@@ -0,0 +1,7 @@
+# https://drive.google.com/file/d/1GUc4CuEKypClgu74BepDCgLqQlUZyjzq/view?usp=drive_link
+
+# https://drive.google.com/file/d/1wu9kF_xJq3hgNFg51BBuDq_oGB68NKVt/view?usp=drive_link
+
+# https://drive.google.com/file/d/10hD4pxTsLguW0vpM2ytZi5E2WNt3nIJT/view?usp=drive_link
+
+# https://drive.google.com/file/d/1D7WgM6T8-GaEv-lEP0doDCCzLf7_uZSr/view?usp=drive_link
