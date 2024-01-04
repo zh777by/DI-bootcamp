db22350e4b80614dc7133383a1e7e4afc7eca7d0
zh777by <byalo.ev@gmail.com>
2024-01-04 14:40:58 +0200

    relevant message

---
+++ Week_7/Day4/class/class.py
@@ -0,0 +1,11 @@
+# https://medium.com/@pankajkumardeora/probability-distribution-functions-pdf-pmf-cdf-ed49e7f5c7f2#:~:text=The%20main%20difference%20between%20the,probabilities%20of%20continuous%20random%20variables
+
+# https://drive.google.com/file/d/1NR9QNy9F3mQFx58tIWZ0Tf3--JWe0R17/view?usp=drive_link
+
+# https://drive.google.com/file/d/1naA6CU2jN6b99NVdgsS7AnRY7n6QyRJ_/view?usp=drive_link
+
+# https://drive.google.com/file/d/1IAlQo7AK6l2jpRXVBJKvHhyN9AKtUZ0m/view?usp=drive_link
+
+# https://drive.google.com/file/d/1XOnYlocZv8NKA5KzhdQxWzVuDUf_3Bpd/view?usp=drive_link
+
+# https://drive.google.com/file/d/17Ys10Y0KgSRGgiIRyUH5tO7bV6xQDqt0/view?usp=drive_link
